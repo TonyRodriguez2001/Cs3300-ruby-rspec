3b2e8a9160d297b6ac42112f5fcba160b3086268
Tony Rodriguez <trodrig4@uccs.edu>
2023-02-20 20:47:19 +0000

    First push

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -4,28 +4,39 @@
 
 def sum arr
   # YOUR CODE HERE
+  return arr.sum
 end
 
 def max_2_sum arr
   # YOUR CODE HERE
+  return arr.max(2).sum
 end
 
 def sum_to_n? arr, n
   # YOUR CODE HERE
+  tOrF = false
+  arr.combinations(2).to_a.each{|i|
+    (i.sum == n) ? tOrF = true : next}
+  return tOrF
 end
 
 # Part 2
 
 def hello(name)
   # YOUR CODE HERE
+  return "Hello, " + name
 end
 
 def starts_with_consonant? s
   # YOUR CODE HERE
+  consonant = false
+  string =~ (/\A[^aeiou]/i) ? consonant = true : consonant = false
+  return consonant
 end
 
 def binary_multiple_of_4? s
   # YOUR CODE HERE
+
 end
 
 # Part 3
